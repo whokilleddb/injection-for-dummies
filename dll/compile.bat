2f8d78dfcaacfbcb7b04678c9e29a8de60f964c4
whokilleddb <whokilleddb@gmail.com>
2023-07-04 00:36:04 +0530

    Injector works

---
+++ dll/compile.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+@REM Compile DLL
+cl.exe /nologo /W0 /Ox /D_USRDLL /D_WINDLL injectme.c /MT /link /DLL /OUT:injectme.dll
+
+@REM Compile Injector
+cl.exe /nologo /Ox /MT /W0 /GS- /DNDEBUG /Tcinjector.c /link /OUT:injector.exe /SUBSYSTEM:CONSOLE /MACHINE:x64
